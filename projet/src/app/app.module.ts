c8bf42fa074133c72201108bf478b921c6019ba5
Celine <celi.caurier@gmail.com>
2020-05-01 14:02:46 +0200

    Menu SideNav

--- projet/src/app/app.module.ts
+++ projet/src/app/app.module.ts
@@ -2,28 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule} from '@angular/forms'
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MaterialModule} from './material.module'
+import {MaterialModule} from './material.module';
+import { ActorComponent } from './actor/actor.component';
+import { EpisodeComponent } from './episode/episode.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ErrorPageComponent 
+    ErrorPageComponent,
+    ActorComponent,
+    EpisodeComponent 
   ],
   imports: [
     MaterialModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
+    FormsModule
    
   ],
   providers: [],
--- projet/src/app/material.module.ts
+++ projet/src/app/material.module.ts
@@ -4,14 +4,19 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
     exports: [
         MatSliderModule,
         MatButtonModule,
         MatIconModule,
-        MatSidenavModule,
         MatSlideToggleModule,
+
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule
     ]
 })
 
